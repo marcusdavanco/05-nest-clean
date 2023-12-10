a29fe7a7d78541ae210776b2cc014cad9bb57ce8
Marcus Davanço <md.daves@gmail.com>
2023-12-10 09:29:10 -0300

    feat: Testando caso de uso de upload

--- src/domain/forum/application/use-cases/upload-and-create-attachments.ts
+++ src/domain/forum/application/use-cases/upload-and-create-attachments.ts
@@ -29,7 +29,7 @@ export class UploadAndCreateAttachmentUseCase {
     fileType,
     body,
   }: UploadAndCreateAttachmentUseCaseRequest): Promise<UploadAndCreateAttachmentUseCaseResponse> {
-    if (!/^image\/(jpeg|png))$|^application\/pdf$/.test(fileType)) {
+    if (!/^image\/(jpeg|png)$|^application\/pdf$/.test(fileType)) {
       return left(new InvalidAttachmentTypeError(fileType))
     }
 
